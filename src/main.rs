ed706cc9ecd1d9708d8aa383b520e3cfacf1948e
John Newby <john@newby.org>
2018-06-08 17:45:47 +0200

    First working thing!

---
+++ src/main.rs
@@ -0,0 +1,171 @@
+#![allow(missing_docs, unused_variables, trivial_casts)]
+
+#[allow(unused_extern_crates)]
+#[macro_use]
+extern crate swagger;
+#[allow(unused_extern_crates)]
+extern crate futures;
+#[macro_use]
+extern crate swagger_client;
+#[allow(unused_extern_crates)]
+extern crate uuid;
+extern crate clap;
+extern crate tokio_core;
+extern crate crypto;
+extern crate hex;
+extern crate blake2b;
+extern crate rand;
+
+extern crate rust_base58;
+use rust_base58::{ToBase58, FromBase58};
+
+extern crate rust_sodium;
+
+use rand::prelude::*;
+
+use swagger::{ContextBuilder, EmptyContext, XSpanIdString, Has, Push, AuthData};
+
+#[allow(unused_imports)]
+use futures::{Future, future, Stream, stream};
+use tokio_core::reactor;
+
+pub mod transaction;
+
+use transaction::KeyPair;
+
+use swagger_client::{ ApiNoContext, ContextWrapperExt,Api, ApiError,
+                      CallContractResponse, CompileContractResponse,
+                      EncodeCalldataResponse,
+                      GetAccountBalanceResponse,
+                      GetAccountTransactionsResponse,
+                      GetAccountsBalancesResponse,
+                      GetBlockByHashResponse,
+                      GetBlockByHeightResponse,
+                      GetBlockGenesisResponse, GetBlockLatestResponse,
+                      GetBlockPendingResponse,
+                      GetCommitmentHashResponse,
+                      GetContractCallFromTxResponse,
+                      GetHeaderByHashResponse,
+                      GetHeaderByHeightResponse, GetInfoResponse,
+                      GetNameResponse, GetPeerKeyResponse,
+                      GetTopResponse, GetTxResponse, GetTxsResponse,
+                      GetVersionResponse, PostBlockResponse,
+                      PostChannelCloseMutualResponse,
+                      PostChannelCloseSoloResponse,
+                      PostChannelCreateResponse,
+                      PostChannelDepositResponse,
+                      PostChannelSettleResponse,
+                      PostChannelSlashResponse,
+                      PostChannelWithdrawalResponse,
+                      PostContractCallResponse,
+                      PostContractCallComputeResponse,
+                      PostContractCreateResponse,
+                      PostNameClaimResponse, PostNamePreclaimResponse,
+                      PostNameRevokeResponse,
+                      PostNameTransferResponse,
+                      PostNameUpdateResponse,
+                      PostOracleExtendResponse,
+                      PostOracleQueryResponse,
+                      PostOracleRegisterResponse,
+                      PostOracleResponseResponse, PostSpendResponse,
+                      PostTxResponse,
+                      GetActiveRegisteredOraclesResponse,
+                      GetBlockNumberResponse,
+                      GetBlockTxsCountByHashResponse,
+                      GetBlockTxsCountByHeightResponse,
+                      GetGenesisBlockTxsCountResponse,
+                      GetLatestBlockTxsCountResponse,
+                      GetOracleQuestionsResponse, GetPeersResponse,
+                      GetPendingBlockTxsCountResponse,
+                      GetPubKeyResponse,
+                      GetTransactionFromBlockHashResponse,
+                      GetTransactionFromBlockHeightResponse,
+                      GetTransactionFromBlockLatestResponse,
+                      GetTxsListFromBlockRangeByHashResponse,
+                      GetTxsListFromBlockRangeByHeightResponse,
+                      PostNameClaimTxResponse,
+                      PostNamePreclaimTxResponse,
+                      PostNameRevokeTxResponse,
+                      PostNameTransferTxResponse,
+                      PostNameUpdateTxResponse,
+                      PostOracleExtendTxResponse,
+                      PostOracleQueryTxResponse,
+                      PostOracleRegisterTxResponse,
+                      PostOracleResponseTxResponse,
+                      PostSpendTxResponse };
+
+use clap::{App,Arg};
+
+pub struct Epoch {
+    client: swagger_client::client::Client,
+    context: swagger_client::Context,
+}
+
+impl Epoch {
+    fn new(base_url: String) -> Epoch {
+        let mut core = reactor::Core::new().unwrap();
+        let client = swagger_client::client::Client::try_new_http(&base_url).expect("Failed to connect");
+        let context = swagger_client::Context::new_with_span_id(self::uuid::Uuid::new_v4().to_string());
+        Epoch { client: client, context: context }
+    }
+
+    fn top(&self) -> i64 {
+        let future_val: std::boxed::Box<futures::Future<Error=swagger_client::ApiError, Item=swagger_client::GetTopResponse>> =
+            self.client.get_top(&self.context);
+        let topresult: swagger_client::GetTopResponse =
+            future_val.wait().unwrap();
+        match topresult {
+            swagger_client::GetTopResponse::SuccessfulOperation(op) =>
+                op.height.unwrap_or(0),
+        }
+    }    
+}
+    
+
+
+fn main() {
+//    let epoch = Epoch::new(String::from("http:localhost:3013"));
+//    println!("Top: {:?}", epoch.top());
+    let key_pair = KeyPair::read_from_files(&String::from("test/keys/testkey.pub"),
+                                            &String::from("test/keys/testkey"),
+                                            &String::from(""));
+    let msg = b"this is a test thing";
+    let mut bytes = key_pair.sign(msg).unwrap();
+    println!("Sig: {:?}", KeyPair::bytes_to_hex(bytes));
+    match key_pair.verify(&bytes, msg) {
+        Ok(foo) => println!("Verification succeeded"),
+        Err(foo) => println!("Verification failed"),
+    }
+    
+    let new_key_pair = KeyPair::generate().unwrap();
+    println!("New key pair: {:?}, {:?}", new_key_pair.get_public_key_readable(),
+             new_key_pair.get_private_key_readable());
+    let msg  =b"this is a test thing";
+    bytes = new_key_pair.sign(msg).unwrap();
+    println!("Signature: {:?}", KeyPair::bytes_to_hex(bytes));
+    match new_key_pair.verify(&bytes, msg) {
+        Ok(foo) => println!("Verification succeeded"),
+        Err(foo) => println!("Verification failed"),
+    }
+    
+    let bytes2 = new_key_pair.sign(msg).unwrap();
+    println!("Signature: {:?}", KeyPair::bytes_to_hex(bytes2));
+    match key_pair.verify(&bytes2, msg) {
+        Ok(foo) => println!("Verification succeeded"),
+        Err(foo) => println!("Verification failed"),
+    }
+    
+    let pub_key_bytes = KeyPair::public_key_bytes_from_readable(new_key_pair.get_public_key_readable());
+    new_key_pair.write_to_files(&String::from("test/keys/new.pub"), &String::from("test/keys/new")).unwrap();
+
+    let loaded_key_pair = KeyPair::read_from_files(&String::from("test/keys/new.pub"),
+                                                   &String::from("test/keys/new"),
+                                                   &String::from(""));
+    match(loaded_key_pair.verify(&bytes, msg)) {
+        Ok(foo) => println!("Verification succeeded"),
+        Err(foo) => println!("Verification failed"),
+    }
+    
+    
+}
+
---
+++ src/transaction.rs
@@ -0,0 +1,139 @@
+#![allow(missing_docs, unused_variables, trivial_casts)]
+
+extern crate rust_base58;
+use rust_base58::{ToBase58, FromBase58};
+
+extern crate crypto;
+use crypto::{ symmetriccipher, buffer, aes, blockmodes };
+
+extern crate hex;
+
+extern crate blake2b;
+use blake2b::blake2b;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Read;
+use std::io::Write;
+
+extern crate rand;
+use rand::prelude::*;
+use rand::OsRng;
+use rand::Rng;
+
+extern crate rust_sodium;
+use rust_sodium::crypto::sign::ed25519;
+
+pub fn hash(input: Vec<u8>) -> Vec<u8> {
+    blake2b(32, &input).to_vec()
+}
+
+pub struct KeyPair {
+    pub public_key: [u8; 32],
+    pub private_key: [u8; 64],
+}
+
+impl KeyPair {
+
+    pub fn new(public_key: [u8; 32], private_key: [u8; 64]) -> KeyPair {
+        KeyPair{public_key: public_key, private_key: private_key}
+    }
+
+    // this one doesn't work, don't know why.
+    pub fn generate_broken() -> Result<KeyPair, ()> {
+        let mut rng = rand::OsRng::new().unwrap();
+        let mut buf = [0u8; 64];
+        rng.fill_bytes(&mut buf);
+        let (priv_key, pub_key) = crypto::ed25519::keypair(&buf);
+        Ok(KeyPair { public_key: pub_key.clone(),
+                     private_key: priv_key.clone(), })
+    }
+
+    // this one does work. 
+    pub fn generate() -> Result<KeyPair, ()> {
+        rust_sodium::init()?;
+        let (_pub, _priv) = rust_sodium::crypto::sign::ed25519::gen_keypair();
+        let mut public_key = [0u8; 32];
+        public_key.copy_from_slice(&_pub[0..32]);
+        let mut private_key = [0u8; 64];
+        private_key.copy_from_slice(&_priv[0..64]);
+        Ok(KeyPair { public_key: public_key, private_key: private_key })
+    }
+    
+    pub fn sign(&self, val: &[u8]) -> Result<[u8; 64], String> {
+        
+        Ok(crypto::ed25519::signature(&val, &self.private_key))
+    }
+
+    pub fn verify(&self, signature: &[u8], message: &[u8])
+                  -> Result<bool, String> {
+        if !crypto::ed25519::verify(&message, &self.public_key, &signature) {
+            return Err(String::from("Verification failed"));
+        }
+        Ok(true)
+    }
+
+    pub fn read_from_files(public_key_file: &String, private_key_file: &String,
+                       password: &String) -> KeyPair {
+        let mut public_key = String::new();
+        let mut private_key = String::new();
+        let mut file = File::open(public_key_file).unwrap();
+        file.read_to_string(&mut public_key);
+        file = File::open(private_key_file).unwrap();
+        file.read_to_string(&mut private_key);
+        KeyPair::from_public_private_key_strings(&public_key, &private_key,
+                                                 &password)
+    }
+
+    pub fn write_to_files(&self, public_key_file: &String, private_key_file: &String)
+                          -> Result<(), ::std::io::Error> {
+        let mut f = File::create(public_key_file)?;
+        f.write_all(self.get_public_key_readable().as_bytes())?;
+        f.flush()?;
+        f = File::create(private_key_file)?;
+        f.write_all(KeyPair::bytes_to_hex(self.private_key).as_bytes());
+        f.flush();
+        Ok(())
+    }
+        
+
+    pub fn from_public_private_key_strings(public_key: &String,
+                                           private_key: &String,
+                                           password: &String)
+                                           -> KeyPair {
+        let bin_public_key = public_key.get(3..).unwrap().from_base58().unwrap();
+        let mut public_key = [0u8; 32];
+        public_key.copy_from_slice(&bin_public_key[0..32]);
+        let bin_private_key = hex::decode(private_key).unwrap();
+        let mut private_key = [0u8; 64];
+        private_key.copy_from_slice(&bin_private_key);
+        KeyPair { private_key: private_key, 
+                  public_key: public_key, }
+    }
+
+    pub fn get_public_key_readable(&self) -> String {
+        format!("{}{}", String::from("ak$"), self.public_key.to_base58())
+    }
+
+    pub fn get_private_key_readable(&self) -> String {
+        KeyPair::bytes_to_hex(self.private_key)
+    }
+
+    pub fn public_key_bytes_from_readable(public_key: String) -> Vec<u8> {
+        let bin_public_key = public_key.get(3..).unwrap().from_base58().unwrap();
+        bin_public_key
+    }
+
+    pub fn to_public_private_key_strings(&self) -> (String, String) {
+        let asc_public_key = self.get_public_key_readable();
+        let asc_private_key = KeyPair::bytes_to_hex(self.private_key);
+        (asc_public_key, asc_private_key)
+    }
+
+    pub fn bytes_to_hex(bytes: [u8; 64]) -> String {
+        let strs: Vec<String> = bytes.iter().map(|b|format!("{:02X}", b)).collect();
+        strs.connect("")
+    }
+
+}
