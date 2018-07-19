b6bedb1031a30aec39d27e545c471e89c3106970
John Newby <john@newby.org>
2018-07-19 12:07:51 +0200

    Broken for getting help

--- migrations/2018-06-28-132752_create_transactions/up.sql
+++ migrations/2018-06-28-132752_create_transactions/up.sql
@@ -2,14 +2,15 @@
 
 CREATE TABLE transactions (
        id SERIAL PRIMARY KEY,
-       block_id INT REFERENCES blocks(id),
-       original_json TEXT NOT NULL,
-       recipient_pubkey VARCHAR(55),
-       amount BIGINT NULL,
-       fee BIGINT,
-       ttl BIGINT,
-       sender VARCHAR(55),
-       payload TEXT NULL    
-
+       block_id INT NOT NULL REFERENCES blocks(id),
+       block_height INT NOT NULL,
+       block_hash VARCHAR(55) NOT NULL,
+       hash VARCHAR(55) NOT NULL,
+       signatures TEXT NOT NULL,
+       tx_type VARCHAR(64) NOT NULL,
+       tx JSONB NOT NULL,
+       valid BOOLEAN NOT NULL DEFAULT TRUE
 );
        
+CREATE INDEX transactions_tx_type_index ON transactions(tx_type);
+
