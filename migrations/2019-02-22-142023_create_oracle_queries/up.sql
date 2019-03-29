1511e7dc14d2082c55d9f3f7762ef3ed0efce6fa
Andrea Giacobino <no.andrea@gmail.com>
2019-03-29 10:33:10 +0100

    fix: constraint violation

--- migrations/2019-02-22-142023_create_oracle_queries/up.sql
+++ migrations/2019-02-22-142023_create_oracle_queries/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE oracle_queries (
        id SERIAL PRIMARY KEY,
        oracle_id VARCHAR(55),
        query_id VARCHAR(255),
-       transaction_id INTEGER NOT NULL REFERENCES transactions(id));
+       transaction_id INTEGER NOT NULL REFERENCES transactions(id) ON DELETE CASCADE);
 
 CREATE INDEX oracle_queries_oracle_id ON oracle_queries(oracle_id);
 CREATE INDEX oracle_queries_query_id ON oracle_queries(query_id);
