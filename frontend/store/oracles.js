70a07fdfa771fd62a1f112bd1dc04f5e81ba6797
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-05-31 13:04:16 +0200

    oracle store

---
+++ frontend/store/oracles.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+export const state = () => ({
+  oracles: {}
+})
+
+export const mutations = {
+  setOracles (state, oracles) {
+    for (let oracle of oracles) {
+      Vue.set(state.oracles, oracle.query_id, oracle)
+    }
+  }
+}
+
+export const actions = {
+  getOracles: async function ({ rootState: { nodeUrl }, commit }, { page, limit }) {
+    try {
+      const oracles = await axios.get(nodeUrl + '/middleware/oracles/all?limit=' + limit + '&page=' + page)
+      commit('setOracles', oracles.data)
+      return oracles.data
+    } catch (e) {
+      console.log(e)
+      commit('catchError', 'Error', { root: true })
+    }
+  }
+}
