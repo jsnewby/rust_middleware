36760437b287bdc29b1eae393ef3152b0b73ff0d
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-07-03 14:51:19 +0200

    fix the name claim tx

--- src/models.rs
+++ src/models.rs
@@ -752,6 +752,9 @@ pub struct InsertableName {
 }
 
 impl InsertableName {
+    
+    pub const NAME_CLAIM_MAX_EXPIRATION: i64 = 50000;
+
     pub fn new(
         _name: &String,
         _name_hash: &String,
@@ -780,10 +783,8 @@ impl InsertableName {
         let _name_hash = super::hashing::get_name_id(&_name).unwrap(); // TODO
         let _tx_hash = transaction.hash.clone();
         let _account_id = transaction.tx["account_id"].as_str()?;
-        let _expires_at = match transaction.tx["ttl"].as_i64() {
-            Some(_ttl) => { _ttl + transaction.block_height as i64 },
-            _ => { 0 }
-        };
+        let _expires_at = InsertableName::NAME_CLAIM_MAX_EXPIRATION;
+        
         Some(InsertableName::new(
             &_name.to_string(),
             &_name_hash,
