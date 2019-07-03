69eb04b04fe3af45c612742b98ed9b82d90ff96b
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-07-03 14:52:27 +0200

    add the height

--- src/models.rs
+++ src/models.rs
@@ -783,8 +783,8 @@ impl InsertableName {
         let _name_hash = super::hashing::get_name_id(&_name).unwrap(); // TODO
         let _tx_hash = transaction.hash.clone();
         let _account_id = transaction.tx["account_id"].as_str()?;
-        let _expires_at = InsertableName::NAME_CLAIM_MAX_EXPIRATION;
-        
+        let _expires_at = InsertableName::NAME_CLAIM_MAX_EXPIRATION + transaction.block_height as i64;
+
         Some(InsertableName::new(
             &_name.to_string(),
             &_name_hash,
