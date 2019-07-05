3cba46e03605b57078256ab3d49bb6b26dcb019e
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-07-05 16:27:26 +0200

    add vue-multiselect

--- frontend/pages/transactions/index.vue
+++ frontend/pages/transactions/index.vue
@@ -5,6 +5,11 @@
       :has-crumbs="true"
       :page="{to: '/transactions', name: 'Transactions'}"
     />
+    <multiselect
+      v-model="value"
+      :options="options"
+      placeholder="Transaction Type"
+    />
     <TxList>
       <TXListItem
         v-for="(item, index) in Object.values(transactions).reverse()"
@@ -22,7 +27,7 @@ import TxList from '../../partials/transactions/txList'
 import TXListItem from '../../partials/transactions/txListItem'
 import PageHeader from '../../components/PageHeader'
 import LoadMoreButton from '../../components/loadMoreButton'
-
+import Multiselect from 'vue-multiselect'
 import { mapState } from 'vuex'
 
 export default {
@@ -31,11 +36,14 @@ export default {
     TxList,
     TXListItem,
     PageHeader,
-    LoadMoreButton
+    LoadMoreButton,
+    Multiselect
   },
   data () {
     return {
-      page: 1
+      page: 1,
+      value: 'All',
+      options: ['SpendTx', 'OracleRegisterTx', 'OracleExtendTx', 'OracleQueryTx', 'OracleRespondTx', 'NamePreclaimTx', 'NameClaimTx', 'NameUpdateTx', 'NameTransferTx', 'NameRevokeTx', 'ChannelCreateTx', 'ContractCallTx', 'ContractCreateTx', 'ChannelDepositTx', 'ChannelWithdrawTx', 'ChannelCloseMutualTx', 'ChannelForceProgressTx', 'ChannelCloseSoloTx', 'ChannelSlashTx', 'ChannelSettleTx', 'ChannelSnapshotSoloTx']
     }
   },
   computed: {
