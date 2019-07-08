5dbace99f3dd3abe3720c6000a0311f53dc89fc7
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-07-08 14:25:06 +0200

    add channel settle tx

---
+++ frontend/partials/transactions/txListType/channelSettleTx.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="transaction">
+    <div class="transaction-main-info">
+      <div class="transaction-main-info-inner">
+        <nuxt-link :to="`/transactions/${transaction.hash}`">
+          <div class="transaction-label">
+            <LabelType
+              :title="transaction.tx.type.replace(/([A-Z])/g, ' $1')"
+              fill="red"
+            />
+          </div>
+        </nuxt-link>
+        <AppDefinition
+          v-if="transaction.tx.time"
+          title="Age"
+        >
+          <Age :time="transaction.tx.time" />
+        </AppDefinition>
+      </div>
+      <div class="transaction-main-info-inner accounts">
+        <AccountGroup>
+          <Account
+            v-if="transaction.tx.from_id"
+            :value="transaction.tx.from_id"
+            title="From Account"
+            icon
+          />
+          <Account
+            v-if="transaction.tx.channel_id"
+            :value="transaction.tx.channel_id"
+            title="channel id"
+            icon
+          />
+        </AccountGroup>
+      </div>
+    </div>
+    <div class="transaction-type-info">
+      <div class="transaction-type-info-item">
+        <AppDefinition
+          title="responder amount final"
+        >
+          <FormatAeUnit :value="transaction.tx.responder_amount_final" />
+        </AppDefinition>
+        <AppDefinition
+          title="initiator amount final"
+        >
+          <FormatAeUnit :value="transaction.tx.initiator_amount_final" />
+        </AppDefinition>
+      </div>
+      <div class="transaction-type-info-item">
+        <AppDefinition
+          v-if="transaction.tx.fee"
+          title="tx fee"
+        >
+          <FormatAeUnit :value="transaction.tx.fee" />
+        </AppDefinition>
+        <AppDefinition
+          v-if="transaction.tx.cost"
+          title="tx cost"
+        >
+          <FormatAeUnit :value="transaction.tx.cost" />
+        </AppDefinition>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AppDefinition from '../../../components/appDefinition'
+import FormatAeUnit from '../../../components/formatAeUnit'
+import AccountGroup from '../../../components/accountGroup'
+import Account from '../../../components/account'
+import Age from '../../../components/age'
+import LabelType from '../../../components/labelType'
+
+export default {
+  name: 'ChannelSettleTx',
+  components: {
+    LabelType,
+    AppDefinition,
+    FormatAeUnit,
+    AccountGroup,
+    Account,
+    Age
+  },
+  props: {
+    transaction: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
