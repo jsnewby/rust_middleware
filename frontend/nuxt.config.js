9a48a7aa4c42a39973ddf034fd52a512bb73c604
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-07-09 12:09:05 +0200

    update name and update favicon path

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -7,14 +7,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'æpp-middleware',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' }
     ]
   },
 
