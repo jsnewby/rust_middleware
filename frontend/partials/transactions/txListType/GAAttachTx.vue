6b6135663e3fca2984537641ecd96da3cd76daaa
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-07-25 11:28:16 +0200

    update ga attach tx

--- frontend/partials/transactions/txListType/GAAttachTx.vue
+++ frontend/partials/transactions/txListType/GAAttachTx.vue
@@ -10,12 +10,6 @@
             />
           </div>
         </nuxt-link>
-        <AppDefinition
-          v-if="transaction.tx.time"
-          title="Age"
-        >
-          <Age :time="transaction.tx.time" />
-        </AppDefinition>
       </div>
       <div class="transaction-main-info-inner accounts">
         <Account
@@ -64,6 +58,7 @@
         <AppDefinition
           v-if="transaction.time"
           title="Time"
+          class="tx-time"
         >
           {{ transaction.time | timestampToUTC }}
         </AppDefinition>
@@ -75,7 +70,6 @@
 import AppDefinition from '../../../components/appDefinition'
 import FormatAeUnit from '../../../components/formatAeUnit'
 import Account from '../../../components/account'
-import Age from '../../../components/age'
 import LabelType from '../../../components/labelType'
 import timestampToUTC from '../../../plugins/filters/timestampToUTC'
 
@@ -85,8 +79,7 @@ export default {
     LabelType,
     AppDefinition,
     FormatAeUnit,
-    Account,
-    Age
+    Account
   },
   filters: {
     timestampToUTC
