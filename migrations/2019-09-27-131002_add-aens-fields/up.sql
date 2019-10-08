fc2255ec621311a326277d4bfe998928f5c587ee
John Newby <john@newby.org>
2019-10-08 15:43:18 +0200

    Fixed stored proc which does name expiration

--- migrations/2019-09-27-131002_add-aens-fields/up.sql
+++ migrations/2019-09-27-131002_add-aens-fields/up.sql
@@ -8,11 +8,11 @@ DECLARE _length NUMERIC = char_length(name) - 4;
 DECLARE multiplier_day INT = 480;
 BEGIN
         CASE
-        WHEN _length > 31 THEN timeout = 0;
+        WHEN _length > 11 THEN timeout = 0;
         WHEN _length > 8 THEN timeout = multiplier_day;
         WHEN _length > 4 THEN
              timeout = multiplier_day * 31;
-        ELSE timeout = multiplier_day;
+        ELSE timeout = multiplier_day * 62;
         END CASE;
 
         RETURN timeout;
