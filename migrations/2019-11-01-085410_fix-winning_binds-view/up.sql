3303ba461c4495fc28c8e397f1a6b2dae08c575f
John Newby <john@newby.org>
2019-11-01 09:54:57 +0100

    Fixed winning bids view

---
+++ migrations/2019-11-01-085410_fix-winning_binds-view/up.sql
@@ -0,0 +1,13 @@
+-- Your SQL goes here
+
+
+CREATE OR REPLACE VIEW winning_bids AS
+SELECT
+	t.tx->>'name' AS name,
+	MAX((t.tx->>'name_fee')::numeric) AS winning_bid
+FROM
+	transactions t JOIN all_names an ON t.tx->>'name' = an.name
+WHERE
+	t.block_height >= an.start_block_height
+GROUP BY
+	tx->>'name';
